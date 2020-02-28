0c41bb50739df0ba67a868ac94bdc8c0be6d7e8e
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-28 10:12:47 +0530

    MODIFIED: Result after 20 day of playing

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -6,22 +6,34 @@ STAKE=100
 BET=1			#STARTING BET FROM RS 1
 #VARIABLE
 isWin=1
-goal=150
-stop=50
-while(($STAKE < $goal && $STAKE > $stop ))
+loose=0
+win=0
+winningTime=0
+loosingTime=0
+for(( day=1; day<=20 ;day++ ))
 do
-	#GENERATING RANDOM VALUE 1 THEN WIN 2 THEN LOOSE
-	random=$((RANDOM%2+1))
-	if(($random == $isWin))
-	then
+	STAKE=100
+	goal=150
+	stop=50
+	while(($STAKE < $goal && $STAKE > $stop ))
+	do
+		#GENERATING RANDOM VALUE 1 THEN WIN 2 THEN LOOSE
+		random=$((RANDOM%2+1))
+		if(($random == $isWin))
+		then
 			STAKE=$(($STAKE+$BET))
 		else
 			STAKE=$(($STAKE-$BET))
-	fi
-done
+		fi
+	done
 	if (( $((STAKE-100))>0 ))
 	then
-		echo "Win"
+		winningTime=$(($winingTime+1))
+		win=$(($win+50))
 	else
-		echo "Loose"
+		loosingTime=$(($loosingTime+1))
+		loose=$(($loose-50))
 	fi
+done
+echo "After 20 day Won $win"
+echo "After 20 day lost  $loose"
