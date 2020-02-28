ebbe6df8a5fc6e3a3df165dfddacd1b175818c1e
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-28 09:30:12 +0530

    MODIFIED: Make  bet win or loose

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,3 +4,12 @@ echo "Welcome to gambling samulation"
 #CONSTANT VARIABLE
 STAKE=100
 BET=1			#STARTING BET FROM RS 1
+#VARIABLE
+isWin=1
+random=$((RANDOM%2+1))
+if(($random == $isWin))
+then
+	echo="Win"
+else
+	echo "Loose"
+fi
