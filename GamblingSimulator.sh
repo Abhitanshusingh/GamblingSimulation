16d9994bb235234c0dc2e03d07c3e09e207340e4
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-28 14:58:28 +0530

    MODIFIED: Finding luckiest day and unluckiest of month

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -2,7 +2,6 @@
 #PRINTING MESSAGE
 echo "Welcome to gambling samulation"
 #CONSTANT VARIABLE
-STAKE=100
 BET=1			#STARTING BET FROM RS 1
 #VARIABLE
 isWin=1
@@ -10,7 +9,6 @@ loose=0
 win=0
 winningTime=0
 loosingTime=0
-declare -A gambling
 for(( day=1; day<=20 ;day++ ))
 do
 	STAKE=100
@@ -29,17 +27,63 @@ do
 	done
 	if (( $((STAKE-100))>0 ))
 	then
-		array[$day]=$(($STAKE-100))
+		#USING ARRAY TO STORE DAILY RECORD
+		array1[$day]=$(($STAKE-100))
 		winningTime=$(($winingTime+1))
 		win=$(($win+50))
-		echo  "Day $day  Won by   : ${array[$day]}"
+		#STORING EACH DAY WINNING
+		array2[$day]=$(($win))
+		#PRINTING DAILY WINNING RECORFD
+		echo  "Day $day  Won by   : ${array1[$day]}"
 	else
-		array[$day]=$(($STAKE-100))
+		#USING ARRAY TO STORE DAILY RECORD
+		array1[$day]=$(($STAKE-100))
 		loosingTime=$(($loosingTime+1))
 		loose=$(($loose-50))
-		echo  "Day $day  Loose by : ${array[$day]}"
-
+		array2[$day]=$(($loose))
+		#PRINTIND DAILY LOOSING RECORD
+		echo  "Day $day  Loose by : ${array1[$day]}"
 	fi
 done
+#RESULT AFTER 20 DAYS
 echo "After 20 day Won $win"
 echo "After 20 day lost  $loose"
+#COUNTING MONEY WIN AND LOSS
+for count in ${!array2[@]}
+do
+	echo "Day $count : ${array2[$count]}" 
+done
+#FINDING MINIMUM AND MAXIMUM VALUE
+minimumMoney=${array2[0]}
+maximumMoney=${array2[0]}
+for cash in ${!array2[@]}
+do
+	if(( $cash > ${array2[cash]} ))
+	then
+		minimumMoney=${array2[cash]}
+	fi
+	if(( $cash < ${array2[cash]} ))
+	then
+
+		maximumMoney=${array2[cash]}
+	fi
+done
+#FOR LUCKIESTDAY AND UNLUCKIEST DAY
+for findDay in ${!array2[@]}
+do
+	if(( ${array2[findDay]}==$maximumMoney ))
+	then
+		echo "Luckiest day is : $findDay"
+	fi
+	if (( ${array2[findDay]}==$minimumMoney))
+   then
+      echo "Unluckiest day is : $findDay"
+   fi
+done
+
+
+
+
+
+
+
