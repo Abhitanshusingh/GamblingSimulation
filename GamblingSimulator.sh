00a6a5bdea219161fe89ff9bc85f1993f138a354
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-28 09:43:07 +0530

    MODIFIED: Won or lost 50% of the stack resign for the day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -6,10 +6,22 @@ STAKE=100
 BET=1			#STARTING BET FROM RS 1
 #VARIABLE
 isWin=1
-random=$((RANDOM%2+1))
-if(($random == $isWin))
-then
-	echo="Win"
-else
-	echo "Loose"
-fi
+goal=150
+stop=50
+while(($STAKE < $goal && $STAKE > $stop ))
+do
+	#GENERATING RANDOM VALUE 1 THEN WIN 2 THEN LOOSE
+	random=$((RANDOM%2+1))
+	if(($random == $isWin))
+	then
+			STAKE=$(($STAKE+$BET))
+		else
+			STAKE=$(($STAKE-$BET))
+	fi
+done
+	if (( $((STAKE-100))>0 ))
+	then
+		echo "Win"
+	else
+		echo "Loose"
+	fi
